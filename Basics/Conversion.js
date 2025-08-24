44e5654d072602384f6ca5fd42279b8e191d4cee
anambibi-357 <anamshareef357@gmail.com>
2025-08-24 09:39:55 +0000

    Conversion

--- Basics/Conversion.js
+++ Basics/Conversion.js
@@ -1,6 +1,22 @@
-let a = 2345.89;
-console.log(typeof a);
-console.log(typeof(a));
- let valueInString = Boolean(2345.89);
- console.log(typeof valueInString);
- console.log(valueInString);
+// let a = 2345.89;
+// console.log(typeof a);
+// console.log(typeof(a));
+//  let valueInString = Boolean(2345.89);
+//  console.log(typeof valueInString);
+//  console.log(valueInString);
+
+
+let gameName = new String('Anam')
+console.log(gameName);
+let heros = ['laiba', 'hiba', 'Imran']
+console.log(heros.slice(1))
+let OneAccount = 'SHAREEF '
+console.log(OneAccount.trim())
+console.log(gameName.length);
+console.log(OneAccount.toLocaleLowerCase())
+
+let rollNo = 'S22BMGMT1M03012'
+console.log(rollNo.replace("S","G"));
+
+
+
