d9788f4ad9d47de454725d5d554a47e98aede343
anambibi-357 <anamshareef357@gmail.com>
2025-08-24 11:48:57 +0000

    Conversion

--- Basics/Conversion.js
+++ Basics/Conversion.js
@@ -6,17 +6,29 @@
 //  console.log(valueInString);
 
 
-let gameName = new String('Anam')
-console.log(gameName);
-let heros = ['laiba', 'hiba', 'Imran']
-console.log(heros.slice(1))
-let OneAccount = 'SHAREEF '
-console.log(OneAccount.trim())
-console.log(gameName.length);
-console.log(OneAccount.toLocaleLowerCase())
+// let gameName = new String('Anam')
+// console.log(gameName);
+// let heros = ['laiba', 'hiba', 'Imran']
+// console.log(heros.slice(1))
+// let OneAccount = 'SHAREEF '
+// console.log(OneAccount.trim())
+// console.log(gameName.length);
+// console.log(OneAccount.toLocaleLowerCase())
 
-let rollNo = 'S22BMGMT1M03012'
-console.log(rollNo.replace("S","G"));
+// let rollNo = 'S22BMGMT1M03012'
+// console.log(rollNo.replace("S","G"));
 
 
+//++++++++++++++++MAth++++++++++++++++
 
+
+// console.log(Math.round(7.98765))
+// console.log(Math.ceil(7.98765))
+// console.log(Math.floor(7.98765))
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10;
+const max = 50;
+console.log(Math.floor(Math.random() * (max-min +1))+min)
